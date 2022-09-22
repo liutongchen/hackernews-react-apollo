c7cec6e93c41403cadf8dfd0f7c61ffa2d961811
Liutong Chen <liutong.clt@gmail.com>
2022-09-22 12:18:23 -0700

    Query and mutation for react components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { render } from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
